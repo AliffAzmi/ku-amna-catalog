a1bb04587bb54b09470fc184e5aef5ce8b1327c1
Aliff Azmi <aliffazmi@Aliffs-MacBook-Air.local>
2022-11-09 17:55:32 +0800

    update cf adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess'
+// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-cloudflare'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +13,6 @@ const config = {
 			postcss: true
 		})
 	]
-};
+}
 
-export default config;
+export default config
